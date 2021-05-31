928287d6be2fb7052daac9f0921b758fe8afe87f
Davi <davilinfo@gmail.com>
2021-05-30 21:21:59 -0300

    adjustments

--- restaurant/backend/index.js
+++ restaurant/backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const http = require('http');
 //const https = require('https');
+//const fs = require('fs')
 const routes = require('./routes/routes');
 const transaction_vector = require('./models/transaction_vector');
 
