c9c92892075b4d3c4f10551dc05fc3203e5d1aeb
Davi <davilinfo@gmail.com>
2021-12-19 08:02:28 -0300

    adding balance recalculation if transaction is not accepted, and moving verification of transaction existance before update sender wallet balance

--- restaurant/backend/controllers/RestaurantController.js
+++ restaurant/backend/controllers/RestaurantController.js
@@ -15,7 +15,7 @@ const transaction_vector = require('../models/transaction_vector');
         return blockchainClient.transactions.get(options);
     }
 
-    var getAccountBalance = async (address) =>{        
+    var getAccountBalance = async (address) =>{
         return blockchainClient.accounts.get({ address, limit: 1});
     }
 
@@ -29,23 +29,23 @@ const transaction_vector = require('../models/transaction_vector');
         return transaction_vector.foods;
     }
 
-    var generateDish = async (foodType) => {        
-        
+    var generateDish = async (foodType) => {
+
         const restaurantAddress = RestaurantInfo.getRestaurantAddress();
         const options = { type: 800, senderId: restaurantAddress,
         sort: 'timestamp:desc', limit: 1 };
         var itemIndex = 0;
-        var food = require("../models/food");   
-        
+        var food = require("../models/food");
+
         const result = await getTransaction(options);
 
         const millisSinceEpoc = Date.now() - 8 - Date.parse(EPOCH_TIME);
         const inSeconds = ((millisSinceEpoc) / 1000).toFixed(0);
-        
+
         itemIndex = result.data.length -1;
-        
-        if (result.data[itemIndex] !== undefined) {                             
-            
+
+        if (result.data[itemIndex] !== undefined) {
+
             for (var index = 0; index < result.data[itemIndex].asset.items.length; index++) {
                 if (result.data[itemIndex].asset.items[index].type == parseInt(foodType)) {
                     food.name = result.data[itemIndex].asset.items[index].name;
@@ -57,53 +57,53 @@ const transaction_vector = require('../models/transaction_vector');
                     food.timestamp = parseInt(inSeconds);
 
                     console.log("food detail: ".concat(food));
-                    return food;  
+                    return food;
                 }
             }
         }
-        return "invalid request";        
+        return "invalid request";
     }
 
-module.exports = {    
-    /* 
+module.exports = {
+    /*
     method type: Get
-    Index method that retrieves menu from the latest MenuTransaction performed for this restaurant 
+    Index method that retrieves menu from the latest MenuTransaction performed for this restaurant
     to perform a MenuTransaction please read the readme file on the backend folder, there is a dish1_commands folder with a menu_transaction.request.js file
-    */ 
+    */
     async index(request, response){
         response.setHeader('Access-Control-Allow-Origin', '*');
-        
+
         const restaurantAddress = RestaurantInfo.getRestaurantAddress();
         const options = { type: 800, senderId: restaurantAddress,
-            sort: 'timestamp:desc', limit: 1 };        
+            sort: 'timestamp:desc', limit: 1 };
         const result = await getTransaction(options);
-        var itemIndex = result.data.length-1;     
+        var itemIndex = result.data.length-1;
 
         if (result.data[itemIndex] !== undefined){
-            console.log(result.data[itemIndex].asset.items);            
+            console.log(result.data[itemIndex].asset.items);
 
             return response.json({
                 status: "ok",
                 result: result.data[itemIndex].asset.items
             })
         }
-        
+
         return response.json({
             status: "error",
             result: null
         })
     },
 
-    /* 
+    /*
     method type: Get
     Returns the food detailed from the respective food type supplied in the URL
     */
     async foodDetail(request, response){
-        response.setHeader('Access-Control-Allow-Origin', '*');        
+        response.setHeader('Access-Control-Allow-Origin', '*');
         var typeId = request.params.id;
         const isInvalidValidRequest = isNaN(typeId) || typeId < 0 || typeId > 50;
-        var rsp = await generateDish(typeId);        
-        console.log(typeId);        
+        var rsp = await generateDish(typeId);
+        console.log(typeId);
         console.log(rsp);
 
         response.json({
@@ -119,28 +119,28 @@ module.exports = {
     async getTransactionById(request, response){
         response.setHeader('Access-Control-Allow-Origin', '*');
 
-        const { transactionId, address } = request.body;        
+        const { transactionId, address } = request.body;
 
         const restaurantAddress = RestaurantInfo.getRestaurantAddress();
-        const options = { type: 820, id: transactionId, limit: 1, senderId: address, recipientId: restaurantAddress };        
+        const options = { type: 820, id: transactionId, limit: 1, senderId: address, recipientId: restaurantAddress };
         var result = await getTransaction(options);
 
         return response.json({ status: "Transaction result", response: result});
     },
 
-    /* 
+    /*
     method type: Post
     Returns transaction detail related to the refund information supplied
     */
     async refund(request, response) {
         response.setHeader('Access-Control-Allow-Origin', '*');
-        
-        const { transactionId, amount, recipientAddress, password } = request.body;        
+
+        const { transactionId, amount, recipientAddress, password } = request.body;
 
         var result = null;
         if (password == "pocFoodRestaurant"){
             const refund = new Refund();
-            result = await refund.commandRefund(transactionId, amount, recipientAddress);                        
+            result = await refund.commandRefund(transactionId, amount, recipientAddress);
 
             return response.json({ status: "Transaction result", response: result});
         }else{
@@ -161,31 +161,31 @@ module.exports = {
 
         return response.json ({ status: "deprecated" });
 
-        const { request_type, encryptedPassphrase, username, table, phone, deliveryaddress } = request.body;        
+        const { request_type, encryptedPassphrase, username, table, phone, deliveryaddress } = request.body;
         const password = 'luxuryRestaurant';
-        
-        const decryptedPassphrase = cryptography.decryptPassphraseWithPassword(encryptedPassphrase, password);        
+
+        const decryptedPassphrase = cryptography.decryptPassphraseWithPassword(encryptedPassphrase, password);
 
         var result = null;
         const isInvalidValidRequest = isNaN(request_type) || request_type < 0 || request_type > 7;
-        console.log("registering payment");        
-        
-        const meat = await generateDish(request_type);           
+        console.log("registering payment");
+
+        const meat = await generateDish(request_type);
         result = await meat.commandFood(decryptedPassphrase, meat, table, request_type, cryptography.encryptPassphraseWithPassword(username, password), cryptography.encryptPassphraseWithPassword(phone, password), cryptography.encryptPassphraseWithPassword(deliveryaddress, password));
-        
+
         return response.json({
             status: isInvalidValidRequest ? "Invalid request type" : "Transaction result",
             response: isInvalidValidRequest ? null : result
-        });        
-    },    
-    
+        });
+    },
+
     async storeQrCodeUrlRestaurant(request, response){
         response.setHeader('Access-Control-Allow-Origin', '*');
-        const { request_type, username, phone, deliveryaddress, observation } = request.body;            
-                
+        const { request_type, username, phone, deliveryaddress, observation } = request.body;
+
         const meat = await generateDish(request_type);
-        const address = RestaurantInfo.getRestaurantAddress();                        
-        const amount = `${transactions.utils.convertLSKToBeddows(meat.amount.toString())}`.toString(); 
+        const address = RestaurantInfo.getRestaurantAddress();
+        const amount = `${transactions.utils.convertLSKToBeddows(meat.amount.toString())}`.toString();
 
         var result = "food://wallet?recipient=".concat(address)
             .concat("&amount=").concat(amount)
@@ -196,19 +196,19 @@ module.exports = {
             .concat("&phone=").concat(phone)
             .concat("&deliveryaddress=").concat(deliveryaddress)
             .concat("&observation=").concat(observation)
-            .concat("&recipientpublickey=").concat(RestaurantInfo.getRestaurantPublicKey());                
+            .concat("&recipientpublickey=").concat(RestaurantInfo.getRestaurantPublicKey());
 
         return response.json({ status: "Waiting payment", response: result});
     },
 
     async storeQrCodeUrlRestaurantAtPlace(request, response){
         response.setHeader('Access-Control-Allow-Origin', '*');
-        const { request_type } = request.body;            
+        const { request_type } = request.body;
 
         const meat = await generateDish(request_type);
-        const address = RestaurantInfo.getRestaurantAddress();                        
+        const address = RestaurantInfo.getRestaurantAddress();
         const amount = `${transactions.utils.convertLSKToBeddows(meat.amount.toString())}`.toString();
-                
+
         var result = null;
         result = "food://wallet?recipient=".concat(address)
             .concat("&amount=").concat(amount);
@@ -227,14 +227,22 @@ module.exports = {
 
         const balance = await getAccountBalance(transaction.senderId);
         if (transaction_vector.foods.length <= 0){
-            foods();
+            await foods();
         }
-        
+
         var foodIndex = transaction_vector.foods.length - 1;
         while (transaction.asset.foodType != transaction_vector.foods[foodIndex].type){
             foodIndex --;
             if (foodIndex < 0){
                 return response.json({ status: "It is not a food transaction offered by this restaurant", response: transaction});
+            }
+        }
+
+        var existe = false;
+        for (var index=0; index < transaction_vector.vector.length; index++){
+            if (transaction_vector.vector[index].id == transaction.id){
+                existe = true;
+                return response.json({ status: "This transaction was already included", response: transaction_vector.vector});
             }
         }
 
@@ -254,14 +262,6 @@ module.exports = {
                         }
                     }
                 }
-            }
-        }
-
-        var existe = false;
-        for (var index=0; index < transaction_vector.vector.length; index++){
-            if (transaction_vector.vector[index].id == transaction.id){
-                existe = true;
-                return response.json({ status: "This transaction was already included", response: transaction_vector.vector});
             }
         }
 
@@ -285,21 +285,30 @@ module.exports = {
 
         if (networkid === 'identifier'){
             const meat = new RestaurantFood();
-                        
-            var result = await meat.receivedSignedTransactionForBroadcast(transaction);                        
+
+            var result = await meat.receivedSignedTransactionForBroadcast(transaction);
+
+            if (result.status != 200){
+
+                for (var j = 0; j < transaction_vector.wallet.length; j ++){
+                        if (transaction_vector.wallet[j].senderId == transaction.senderId){
+                                transaction_vector.wallet[j].balance += (parseFloat(transaction.amount) + parseFloat(transaction.fee));
+                        }
+                }
+            }
 
             return response.json({ status: "Transaction result", response: result});
         }else{
             return response.json({ status: "Transaction not completed", response: null});
         }
-    },    
+    },
 
     async storePaymentWithPassphrase(request, response){
-        response.setHeader('Access-Control-Allow-Origin', '*');        
-        
-        const { transaction, networkid } = request.body;                                    
+        response.setHeader('Access-Control-Allow-Origin', '*');
 
-        if (networkid === 'identifier'){                                                      
+        const { transaction, networkid } = request.body;
+
+        if (networkid === 'identifier'){
 
             var txFood = new FoodTransaction({
                 asset: {
@@ -313,9 +322,9 @@ module.exports = {
                     clientData: transaction.asset.clientData,
                     clientNonce: transaction.asset.clientNonce,
                     key: transaction.asset.key,
-                    keynonce: transaction.asset.keynonce,  
-                    clientpublickey: transaction.asset.clientpublickey              
-                },    
+                    keynonce: transaction.asset.keynonce,
+                    clientpublickey: transaction.asset.clientpublickey
+                },
                 amount: transaction.amount,
                 recipientId: transaction.recipientId, //restaurant lisk address
                 timestamp: transaction.timestamp
@@ -326,7 +335,7 @@ module.exports = {
 
             txFood.sign(cryptography.decryptMessageWithPassphrase(transaction.asset.key, transaction.asset.keynonce, RestaurantInfo.getRestaurantPassphrase(), transaction.asset.clientpublickey));
 
-            const meat = new RestaurantFood();            
+            const meat = new RestaurantFood();
             var result = await meat.receivedSignedTransactionForBroadcast(txFood);
 
             return response.json({ status: "Transaction result", response: result});
@@ -337,7 +346,7 @@ module.exports = {
 
     async cryptographyText(request, response){
         response.setHeader('Access-Control-Allow-Origin', '*');
-        
+
         const { text } = request.body;
 
         var encryptedText = RestaurantInfo.getCryptographedMessage(text);
